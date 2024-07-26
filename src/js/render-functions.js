79f2a7a31cb72a24bdb16c6a438ce0d2fe1d9a73
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2024-07-27 01:42:53 +0300

    add

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -2,10 +2,10 @@
 
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
 export function renderImages(images) {
   const imageContainer = document.getElementById('image-container');
   const loader = document.querySelector('.loader');
-  loader.style.display = 'block';
   imageContainer.innerHTML = '';
   setTimeout(() => {
     if (images.length > 0) {
@@ -13,8 +13,8 @@ export function renderImages(images) {
         image => `
         <li class="gallery-link"><a class="card-link" href="${image.largeImageURL}" data-lightbox="gallery" data-title="${image.tags}">
         <div class="card">
-          <img class="image" src="${image.webformatURL}" alt="${image.tags}" onload="hideLoader(this)" onerror="hideLoader(this)">
-    
+          <img class="image" src="${image.webformatURL}" alt="${image.tags}">
+          <span class="loader"></span>
           <div class="card-points">
             <div class="card-text"><p>Likes: ${image.likes}</p></div>
             <div class="card-text"><p>Views: ${image.views}</p></div>
@@ -27,12 +27,21 @@ export function renderImages(images) {
     `
       );
       imageContainer.innerHTML = imageCards.join('');
-
       const lightbox = new SimpleLightbox('[data-lightbox="gallery"]');
       lightbox.refresh();
     } else {
       imageContainer.innerHTML = '';
     }
-    loader.style.display = 'none';
-  }, 2000);
+    const imageEl = document.querySelectorAll('.image');
+    imageEl.forEach(img => {
+      img.addEventListener('load', () => hideLoader(img));
+      img.addEventListener('error', () => hideLoader(img));
+    });
+  }, 1000);
+  function hideLoader(img) {
+    const loader = img.parentElement.querySelector('.loader');
+    if (loader) {
+      loader.style.display = 'none';
+    }
+  }
 }
