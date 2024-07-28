b4f5b91cec3ad6e1892edb53a6046746de5ef398
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2024-07-28 21:53:19 +0300

    add

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -14,9 +14,7 @@ export function renderImages(images) {
       <li class="gallery-link">
         <a class="card-link" href="${image.largeImageURL}" data-lightbox="gallery" data-title="${image.tags}">
           <div class="card">
-          
-            <img class="image" src="${image.webformatURL}" alt="${image.tags}">
-            
+            <img class="image" src="${image.webformatURL}" alt="${image.tags}" loading="lazy>
             <div class="card-points">
               <div class="card-text"><p>Likes: ${image.likes}</p></div>
               <div class="card-text"><p>Views: ${image.views}</p></div>
@@ -32,6 +30,6 @@ export function renderImages(images) {
     const lightbox = new SimpleLightbox('[data-lightbox="gallery"]');
     lightbox.refresh();
   } else {
-    imageContainer.innerHTML = '';
+    imageContainer.innerHTML = ''; // Повідомлення, якщо немає зображень
   }
 }
